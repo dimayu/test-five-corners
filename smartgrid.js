f4815b5c0c4c4d2ca8cf15a9becc0e3495781d0e
damir <dimayu82@gmail.com>
2023-01-16 18:53:30 +0300

    fix README

--- smartgrid.js
+++ smartgrid.js
@@ -3,19 +3,23 @@ let smartgrid = require('smart-grid');
 let settings = {
     outputStyle: 'scss',
     columns: 12,
-    offset: '0',
+    offset: '0px',
     mobileFirst: false,
     container: {
         maxWidth: '1600px',
         fields: '200px'
     },
     breakPoints: {
-        lm: {
-            width: '1024px',
+        xlm: {
+            width: '1500px',
             fields: '50px'
         },
+        lm: {
+            width: '1240px',
+            fields: '20px'
+        },
         md: {
-            width: '940px',
+            width: '980px',
             fields: '25px'
         },
         sm: {
